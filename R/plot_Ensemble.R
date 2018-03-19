f7a8fa1e74348d0cc96ea55712c551945aaecf47
maierhofert <thomasmaierhofer@hotmail.de>
2018-03-19 14:55:23 -0700

    create PD plots for RFE

---
+++ R/plot_Ensemble.R
@@ -0,0 +1,188 @@
+# Show model weights in LCE and RFE
+library("mlr")
+library("dplyr")
+library("ggplot2")
+
+# load BeetleFly example data
+data("BeetleFly", package = "classiFunc")
+BeetleFly = BeetleFly[,-ncol(BeetleFly)]
+# # export BeetleFly data to mlr data format
+fdata = makeFunctionalData(BeetleFly, exclude.cols = c("target"))
+
+# create mlr task from data
+task = makeClassifTask(data = fdata, target = "target")
+# task = subsetTask(task, c(F, T))
+set.seed(1234)
+
+# define base learners
+b.lrn1 = makeLearner("classif.classiFunc.knn",
+                     id = "L2",
+                     par.vals = list(metric = "L2", knn = 3), 
+                     predict.type = "prob")
+
+library("dtw")
+b.lrn2 = makeLearner("classif.classiFunc.knn", 
+                     id = "dtw",
+                     par.vals = list(metric = "dtw", knn = 3), 
+                     predict.type = "prob")
+
+b.lrn3 = makeLearner("classif.classiFunc.knn", 
+                     id = "globMax",
+                     par.vals = list(metric = "globMax", knn = 3), 
+                     predict.type = "prob")
+
+b.lrn4 = makeLearner("classif.classiFunc.knn", 
+                     id = "random",
+                     par.vals = list(metric = "custom.metric", 
+                                     knn = 3,
+                                     custom.metric = function(x, y) runif(1)), 
+                     predict.type = "prob")
+
+# create LCE
+# use 10 fold CV (default is leave-one-out-CV) for faster run time.
+LCE.lrn = makeStackedLearner(
+  base.learners = list(b.lrn1, b.lrn2, b.lrn3, b.lrn4), 
+  # base.learners = list(b.lrn1, b.lrn3), 
+  predict.type = "prob", 
+  resampling = makeResampleDesc("CV", iters = 10L),
+  method = "classif.bs.optimal")
+
+# create TE (tree ensemble)
+TE.lrn = makeStackedLearner(
+  base.learners = list(b.lrn1, b.lrn2, b.lrn3, b.lrn4), 
+  # base.learners = list(b.lrn1, b.lrn3), 
+  predict.type = "prob",
+  resampling = makeResampleDesc("CV", iters = 10L),
+  method = "stack.cv",
+  super.learner = "classif.ctree")
+
+# create RFE
+RFE.lrn = makeStackedLearner(
+  base.learners = list(b.lrn1, b.lrn2, b.lrn3, b.lrn4), 
+  # base.learners = list(b.lrn1, b.lrn3), 
+  predict.type = "prob",
+  resampling = makeResampleDesc("CV", iters = 10L),
+  method = "stack.cv",
+  super.learner = "classif.randomForest")
+
+# train models on the training data
+set.seed(1)
+LCE.m = train(LCE.lrn, task = task)
+TE.m = train(TE.lrn, task = task)
+RFE.m = train(RFE.lrn, task = task)
+
+
+
+# # TE
+# # classification tree
+# tree = TE.m$learner.model$super.model
+# # pdf("Grafiken/tree_strucplot.pdf", 
+# #     width = pdf.width, height = pdf.height)
+# plot(tree$learner.model,
+#      inner_panel = node_inner(tree$learner.model,
+#                               abbreviate = FALSE,            # short variable names
+#                               pval = FALSE,                 # no p-values
+#                               id = FALSE),
+#      terminal_panel = node_terminal(tree$learner.model, 
+#                                     abbreviate = TRUE,
+#                                     digits = 1,                   # few digits on numbers
+#                                     fill = c("white"),            # make box white not grey
+#                                     id = FALSE))
+# dev.off()
+# 
+# 
+# # tree.plot.raw = plotLearnerPrediction(tree$learner, mod.task, features = c("glucose", "BMI"))
+# # 
+# # tree.plot = tree.plot.raw + 
+# #   ggtitle("") +
+# #   scale_fill_manual(values = c("firebrick3", "steelblue3")) +
+# #   ylab("body mass index") + 
+# #   xlab("plasma glucose") +
+# #   mytheme
+# # tree.plot
+# # ggsave("Grafiken/tree_plot.pdf", tree.plot,
+# #        height = pdf.height, width = pdf.width)
+
+mod = RFE.m
+mod$learner.model$super.model
+varImpPlot(mod$learner.model$super.model$learner.model)
+
+base.lrn = c("L2", "globMax")
+# TE.m$learner.model$base.models
+# plotEnsemblePrediction = function(mod, base.lrn) {
+# create fake data for all combinations
+
+# TODO use CV or check out how it works internally
+base.preds = lapply(mod$learner.model$base.models, predict, task = task)
+base.preds = lapply(base.preds, function(x) x$data)
+colnames(base.preds[[1]]) = paste0("L2_", colnames(base.preds[[1]]))
+colnames(base.preds[[2]]) = paste0("dtw_", colnames(base.preds[[2]]))
+colnames(base.preds[[3]]) = paste0("globMax_", colnames(base.preds[[3]]))
+colnames(base.preds[[4]]) = paste0("random_", colnames(base.preds[[4]]))
+
+dat = cbind(base.preds[[1]], base.preds[[2]], base.preds[[3]], base.preds[[4]])
+
+
+# add ensemble prediction
+ens.pred.df = predict(mod, task = task)$data
+dat = cbind(dat, ens.pred.df)
+
+
+# aggregate over all data with same prbabilites in base learners
+L2_dtw_dat = dat %>%
+  group_by(L2_prob.1, dtw_prob.1) %>%
+  summarize(prob.1 = mean(prob.1))
+ggplot(L2_dtw_dat, aes(x = L2_prob.1, y = dtw_prob.1, fill = prob.1)) +
+  geom_raster(interpolate = F) +
+  scale_fill_gradient2("P(Beetle)\n", midpoint = 0.5, limits = c(0, 1),
+                       high = "dodgerblue", low = "red")
+
+L2_random_dat = dat %>%
+  group_by(L2_prob.1, random_prob.1) %>%
+  summarize(prob.1 = mean(prob.1))
+ggplot(L2_random_dat, aes(x = L2_prob.1, y = random_prob.1, fill = prob.1)) +
+  geom_raster(interpolate = F) +
+  scale_fill_gradient2("P(Beetle)\n", midpoint = 0.5, limits = c(0, 1),
+                       high = "dodgerblue", low = "red")
+
+dtw_random_dat = dat %>%
+  group_by(dtw_prob.1, random_prob.1) %>%
+  summarize(prob.1 = mean(prob.1))
+ggplot(dtw_random_dat, aes(x = dtw_prob.1, y = random_prob.1, fill = prob.1)) +
+  geom_raster(interpolate = F) +
+  scale_fill_gradient2("P(Beetle)\n", midpoint = 0.5, limits = c(0, 1),
+                       high = "dodgerblue", low = "red")
+
+dtw_globMax_dat = dat %>%
+  group_by(dtw_prob.1, globMax_prob.1) %>%
+  summarize(prob.1 = mean(prob.1))
+ggplot(dtw_globMax_dat, aes(x = dtw_prob.1, y = globMax_prob.1, fill = prob.1)) +
+  geom_raster(interpolate = F) +
+  scale_fill_gradient2("P(Beetle)\n", midpoint = 0.5, limits = c(0, 1),
+                       high = "dodgerblue", low = "red")
+
+L2_globMax_dat = dat %>%
+  group_by(L2_prob.1, globMax_prob.1) %>%
+  summarize(prob.1 = mean(prob.1))
+ggplot(L2_globMax_dat, aes(x = L2_prob.1, y = globMax_prob.1, fill = prob.1)) +
+  geom_raster(interpolate = F) +
+  scale_fill_gradient2("P(Beetle)\n", midpoint = 0.5, limits = c(0, 1),
+                       high = "dodgerblue", low = "red")
+
+
+
+# # }
+# # RFE
+# # plot learner prediction
+# rf.lrn = RFE.m$learner.model$super.model
+# 
+# rf.plot.raw = plotLearnerPrediction(rf.lrn, mod.task, features = c("glucose", "BMI"))
+# rf.plot = rf.plot.raw + 
+#   ggtitle("") +
+#   scale_fill_manual(values = c("firebrick3", "steelblue3")) +
+#   ylab("body mass index") + 
+#   xlab("plasma glucose") +
+#   mytheme
+# rf.plot
+# # ggsave("Grafiken/rf_plot.pdf", rf.plot,
+# #        height = pdf.height, width = pdf.width)
---
+++ R/plot_Ensemble_PartialDependence.R
@@ -0,0 +1,118 @@
+###
+learner = RFE.lrn
+task = task
+
+
+# create partial dependence plots for RFE
+
+####
+# copy & paste from mlr
+# stackCV = function(learner, task) {
+td = getTaskDesc(task)
+type = ifelse(td$type == "regr", "regr",
+              ifelse(length(td$class.levels) == 2L, "classif", "multiclassif"))
+bls = learner$base.learners
+use.feat = learner$use.feat
+# cross-validate all base learners and get a prob vector for the whole dataset for each learner
+base.models = probs = vector("list", length(bls))
+rin = makeResampleInstance(learner$resampling, task = task)
+for (i in seq_along(bls)) {
+  bl = bls[[i]]
+  r = resample(bl, task, rin, show.info = FALSE)
+  probs[[i]] = mlr:::getResponse(r$pred, full.matrix = FALSE)
+  # also fit all base models again on the complete original data set
+  base.models[[i]] = train(bl, task)
+}
+names(probs) = names(bls)
+
+if (type == "regr" || type == "classif") {
+  probs = as.data.frame(probs)
+} else {
+  probs = as.data.frame(lapply(probs, function(X) X)) #X[, -ncol(X)]))
+}
+
+# add true target column IN CORRECT ORDER
+tn = getTaskTargetNames(task)
+test.inds = unlist(rin$test.inds)
+
+pred.train = as.list(probs[order(test.inds), , drop = FALSE])
+
+probs[[tn]] = getTaskTargets(task)[test.inds]
+
+# now fit the super learner for predicted_probs --> target
+probs = probs[order(test.inds), , drop = FALSE]
+# if (use.feat) {
+#   # add data with normal features IN CORRECT ORDER
+#   feat = getTaskData(task)#[test.inds, ]
+#   feat = feat[, !colnames(feat) %in% tn, drop = FALSE]
+#   pred.data = cbind(probs, feat)
+#   super.task = makeSuperLearnerTask(learner, data = pred.data, target = tn)
+# } else {
+super.task = mlr:::makeSuperLearnerTask(learner, data = probs, target = tn)
+# }
+super.model = train(learner$super.learner, super.task)
+# list(method = "stack.cv", base.models = base.models,
+#      super.model = super.model, pred.train = pred.train)
+# }
+
+
+
+################################################################################
+# partial dependence
+name = "RFE"
+
+# create partial dependence data
+# L2 dtw
+pd_dat_L2_dtw = generatePartialDependenceData(obj = super.model, input = super.task, 
+                                              features = c("L2", "dtw"), interaction = TRUE)
+plotPartialDependence(pd_dat_L2_dtw, geom = "tile") +
+  scale_fill_gradient2("P(Beetle)\n", midpoint = 0.5, limits = c(0, 1),
+                       high = "dodgerblue", low = "red") +
+  facet_null()
+ggsave(paste0("Plots/PD_", name, "_ens_L2_dtw.pdf"), width = 6, height = 5)
+
+# L2 random
+pd_dat_L2_random = generatePartialDependenceData(obj = super.model, input = super.task, 
+                                                 features = c("L2", "random"), interaction = TRUE)
+plotPartialDependence(pd_dat_L2_random, geom = "tile") +
+  scale_fill_gradient2("P(Beetle)\n", midpoint = 0.5, limits = c(0, 1),
+                       high = "dodgerblue", low = "red") +
+  facet_null()
+ggsave(paste0("Plots/PD_", name, "_ens_L2_random.pdf"), width = 6, height = 5)
+
+# L2 globMax
+pd_dat_L2_globMax = generatePartialDependenceData(obj = super.model, input = super.task, 
+                                                  features = c("L2", "globMax"), interaction = TRUE)
+plotPartialDependence(pd_dat_L2_globMax, geom = "tile") +
+  scale_fill_gradient2("P(Beetle)\n", midpoint = 0.5, limits = c(0, 1),
+                       high = "dodgerblue", low = "red") +
+  facet_null()
+ggsave(paste0("Plots/PD_", name, "_ens_L2_globMax.pdf"), width = 6, height = 5)
+
+# dtw random
+pd_dat_random_dtw = generatePartialDependenceData(obj = super.model, input = super.task, 
+                                                  features = c("random", "dtw"), interaction = TRUE)
+plotPartialDependence(pd_dat_random_dtw, geom = "tile") +
+  scale_fill_gradient2("P(Beetle)\n", midpoint = 0.5, limits = c(0, 1),
+                       high = "dodgerblue", low = "red") +
+  facet_null()
+ggsave(paste0("Plots/PD_", name, "_ens_random_dtw.pdf"), width = 6, height = 5)
+
+# dtw globMax
+pd_dat_dtw_globMax = generatePartialDependenceData(obj = super.model, input = super.task, 
+                                                   features = c("dtw", "globMax"), interaction = TRUE)
+plotPartialDependence(pd_dat_dtw_globMax, geom = "tile") +
+  scale_fill_gradient2("P(Beetle)\n", midpoint = 0.5, limits = c(0, 1),
+                       high = "dodgerblue", low = "red") +
+  facet_null()
+ggsave(paste0("Plots/PD_", name, "_ens_dtw_globMax.pdf"), width = 6, height = 5)
+
+# random globMax
+pd_dat_random_globMax = generatePartialDependenceData(obj = super.model, input = super.task, 
+                                                      features = c("random", "globMax"), interaction = TRUE)
+plotPartialDependence(pd_dat_random_globMax, geom = "tile") +
+  scale_fill_gradient2("P(Beetle)\n", midpoint = 0.5, limits = c(0, 1),
+                       high = "dodgerblue", low = "red") +
+  facet_null()
+ggsave(paste0("Plots/PD_", name, "_ens_random_globMax.pdf"), width = 6, height = 5)
+
