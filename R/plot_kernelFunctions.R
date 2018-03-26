70a322705d03e8c1f8acc56bbf1be37bf2911a00
maierhofert <thomasmaierhofer@hotmail.de>
2018-03-26 14:39:32 -0700

    create plots for kernel functions

---
+++ R/plot_kernelFunctions.R
@@ -0,0 +1,32 @@
+# create plot for kernel functions in paper
+library("classiFunc")
+library("fda.usc")
+library("ggplot2")
+library("reshape2")
+
+kers = kerChoices()
+kers = kers[kers %in% c("Ker.norm", "Ker.cos", "Ker.epa", "Ker.tri", "Ker.quar",
+                        "Ker.unif")]
+
+# create data to be plotted
+x = seq(-1.1, 1.1, by = 0.01)
+dat = sapply(kers, function(ker) {
+  do.call(ker, list(x))
+})
+dat = as.data.frame(dat)
+colnames(dat) = c("Gaussian", "Cosine", "Epanechnikov", "Triweight", 
+                  "Quartic", "Uniform")
+dat$x = x
+
+# convert to long format for ggplot
+longDat <- melt(dat, id.vars = c("x"))
+
+
+
+# plot
+ggplot(longDat) +
+  geom_line(aes(x = x, y = value, color = variable), size = 1) +
+  scale_color_discrete("") +
+  xlab("u") +
+  ylab("K(u)")
+ggsave("Plots/kernelFunctions.pdf", width = 7, height = 4)
