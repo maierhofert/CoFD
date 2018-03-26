01434b07da19e1001c989e53de492dc6c65b0e26
maierhofert <thomasmaierhofer@hotmail.de>
2018-03-26 16:59:52 -0700

    update beetlefly plots

--- R/plot_beetlefly.R
+++ R/plot_beetlefly.R
@@ -17,44 +17,89 @@ b = abs(b - 1)
 ocb = ocontour(b)
 
 # plot outline
-jpeg("Plots/Beetle1_outline.JPG")
-plot(ocb[[1]], type = 'l')
-points(ocb[[1]], col = 2)
+fac = 9
+c.lab = 3
+jpeg("Plots/Beetle1_outline.JPG", width = 58*fac, height = 103*fac)
+par(mar = c(5, 5, 4, 2) + 0.1)
+plot(-ocb[[1]], type = 'l', xlab = "x", ylab = "y", cex.lab = c.lab)
+points(-ocb[[1]], col = 2)
 
 # find center (maybe use median instead for robustness)
-center = colMeans(ocb$`1`)
-points(center[1], center[2])
+center = colMeans(-ocb$`1`)
+points(center[1], center[2], pch = "C", cex = 2)
 
 # mark "start point"
-points(ocb$`1`[1,1], ocb$`1`[1,2], col = "green")
+points(-ocb$`1`[1,1], -ocb$`1`[1,2], pch = "S", col = "darkgreen", cex = 2)
 dev.off()
 
-# plot distance to outline
-jpeg("Plots/Beetle1_dist_outline.JPG")
-plot(dist_to_center)
-dev.off()
 
-# for one fly
-f = x[1:80, 122:234, 1]
-writeImage(f, "Plots/Fly1.PNG")
-# invert colors
-f = abs(f - 1)
-f = rotate(f, angle = 280)
-
-display(x)
-oc = ocontour(x)
-plot(oc[[1]], type = 'l')
-points(oc[[1]], col = 2)
-
-# find center (maybe use median instead for robustness)
-center = colMeans(oc$`1`)
-points(center[1], center[2])
-
-# mark "start point"
-points(oc$`1`[1,1], oc$`1`[1,2], col = "green")
 
 # find distance to centerpoint
 # apply(oc$`1`[1:2,], 1, function(x) {proxy::dist(x, center)})
 dist_to_center = proxy::dist(oc$`1`, matrix(center, nrow = 1), by_rows = TRUE)
 
-plot(dist_to_center)
+# plot distance to outline
+jpeg("Plots/Beetle1_dist_outline.JPG", width = 2*58*fac, height = 103*fac)
+par(mar = c(5, 5, 4, 2) + 0.1)
+plot(dist_to_center, ylab = "Distance to center", xlab = "", cex.lab = c.lab)
+points(0, dist_to_center[1], pch = "S", col = "darkgreen", cex = 2)
+dev.off()
+
+
+
+# ##############################################################################
+
+# for one fly
+f = x[1:80, 122:234, 1]
+writeImage(f, "Plots/Fly1.JPG")
+
+# invert colors
+f = abs(f - 1)
+# f = rotate(f, angle = 280)
+
+display(f)
+oc = ocontour(f)
+
+# plot(oc[[1]], type = 'l')
+# points(oc[[1]], col = 2)
+# 
+# # find center (maybe use median instead for robustness)
+# center = colMeans(oc$`1`)
+# points(center[1], center[2])
+# 
+# # mark "start point"
+# points(oc$`1`[1,1], oc$`1`[1,2], col = "green")
+# 
+# # find distance to centerpoint
+# # apply(oc$`1`[1:2,], 1, function(x) {proxy::dist(x, center)})
+# dist_to_center = proxy::dist(oc$`1`, matrix(center, nrow = 1), by_rows = TRUE)
+# 
+# plot(dist_to_center)
+
+
+# plot outline
+fac = 9
+jpeg("Plots/Fly1_outline.JPG", width = 80*fac, height = 113*fac)
+par(mar = c(5, 5, 4, 2) + 0.1)
+plot(oc[[1]], type = 'l', xlab = "x", ylab = "y", cex.lab = c.lab)
+points(oc[[1]], col = 2)
+
+# find center (maybe use median instead for robustness)
+center = colMeans(oc$`1`)
+points(center[1], center[2], pch = "C", cex = 2)
+
+# mark "start point"
+points(oc$`1`[1,1], oc$`1`[1,2], pch = "S", col = "darkgreen", cex = 2)
+dev.off()
+
+# find distance toc enter
+dist_to_center = proxy::dist(oc$`1`, matrix(center, nrow = 1), by_rows = TRUE)
+
+# plot distance to outline
+jpeg("Plots/Fly1_dist_outline.JPG", width = 2 * 63*fac, height = 113*fac)
+par(mar = c(5, 5, 4, 2) + 0.1)
+plot(dist_to_center, ylab = "Distance to center", xlab = "", cex.lab = c.lab)
+points(0, dist_to_center[1], pch = "S", col = "darkgreen", cex = 2)
+dev.off()
+
+
